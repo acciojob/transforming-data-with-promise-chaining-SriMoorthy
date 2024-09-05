7742cbcfb3ee4138af9a7d0cdf76d4b682f741f8
SriMoorthy <srimoorthy27@gmail.com>
2024-09-05 16:19:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
 //your JS code here. If required.
 function numberOperation(){
     const input = parseInt(document.getElementById("ip").value)
-    let promise = new Promise(function(resolve){
+    new Promise(function(resolve){
         setTimeout(()=>{
             document.getElementById("output").textContent = `Result: ${input}`
             resolve(input);
-            return input;
-        },2000)
-    });
-
-    promise.then(function(num1){
-        setTimeout(()=>{
-            const result = num1-3
-            document.getElementById("output").textContent = `Result: ${result}`
-            return result
         },2000)
     })
 
-    promise.then(function(num2){
-        setTimeout(()=>{
+    .then(function(num1){
+        return new Promise(function(resolve){
+            setTimeout(()=>{
+            const result = num1
+            document.getElementById("output").textContent = `Result: ${result}`
+            resolve(result)
+            },2000)
+       })
+    })
+
+    .then(function(num2){
+        return new Promise(function(resolve){
+            setTimeout(()=>{
             const result = num2*2
             document.getElementById("output").textContent = `Result: ${result}`
-            return result
-        },1000)
+            resolve(result)
+            },1000)
+       })
     })
+
+    .then(function(num3){
+        return new Promise(function(resolve){
+            setTimeout(()=>{
+            const result = num3-3
+            document.getElementById("output").textContent = `Result: ${result}`
+            resolve(result)
+            },1000)
+       })
+    })
+
+    .then(function(num4){
+        return new Promise(function(resolve){
+            setTimeout(()=>{
+            const result = num4/2
+            document.getElementById("output").textContent = `Result: ${result}`
+            resolve(result)
+            },1000)
+       })
+    })
+
+    .then(function(num5){
+        return new Promise(function(resolve){
+            setTimeout(()=>{
+            const result = num5+10
+            document.getElementById("output").textContent = `Result: ${result}`
+            resolve(result)
+            },1000)
+       })
+    })
+ 
 }
