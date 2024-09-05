269986c6a91ce4b9511a5abd1f1bce486a3b7f4f
SriMoorthy <srimoorthy27@gmail.com>
2024-09-05 14:39:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+function numberOperation(){
+    const input = parseInt(document.getElementById("ip").value)
+    let promise = new Promise(function(resolve){
+        setTimeout(()=>{
+            document.getElementById("output").textContent = `Result ${input}`
+            resolve(input);
+            return input;
+        },2000)
+    });
+
+    promise.then(function(num1){
+        setTimeout(()=>{
+            const result = num1-3
+            document.getElementById("output").textContent = `Result ${result}`
+            return result
+        },2000)
+    })
+
+    promise.then(function(num2){
+        setTimeout(()=>{
+            const result = num2*2
+            document.getElementById("output").textContent = `Result ${result}`
+            return result
+        },1000)
+    })
+}
